482f272e25a4e666114feda321dba1487d749ed2
七月十五 <194962218@qq.com>
2021-09-23 17:49:15 +0800

    fix:解决调整拖拽结束时居中时组件闪动 BUG

--- src/packages/EditorBlock.tsx
+++ src/packages/EditorBlock.tsx
@@ -17,6 +17,7 @@ export const VisualEditorBlock: React.FC<{
         return {
             top: `${props.block.top}px`,
             left: `${props.block.left}px`,
+            opacity: props.block.adjustPosition ? 0 : "", // 解决调整拖拽结束时居中时组件闪动 BUG
         }
     }, [props.block.top, props.block.left]);
 
